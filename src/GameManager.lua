39325595f5d8f869396859cf10a846c63687028f
yPixelar <isaveliara.c@gmail.com>
2024-10-11 19:25:40 -0300

    atualização no GameMaster que esqueci de colocar

--- src/GameManager.lua
+++ src/GameManager.lua
@@ -1,40 +1,70 @@
+--Esse arquivo é um script no ServerScriptService
+
+local GameManager = {}
 local ReplicatedStorage = game:GetService("ReplicatedStorage")
-local Players = game:GetService("Players")
+local GameMaster = require(game.ServerScriptService.GameMaster) -------era pra usar isso
+local currentMinigame --armazena o minigame atual
+local onEndConnection --armazena a conexão do evento OnEnd
 
-local startMinigameEvent = ReplicatedStorage:WaitForChild("Events"):WaitForChild("StartMinigame")
-local announceEvent = ReplicatedStorage:WaitForChild("Events"):WaitForChild("AnnounceMinigame")
+--minigames disponíveis, adicionar iteração para definir eles automaticamente...
+local availableMinigames = {"HelloWorldGame","RaceGame"}
 
-local minigames = {
-	require(script.Parent:WaitForChild("SnakeGame")),
-	require(script.Parent:WaitForChild("Race2")),
-	require(script.Parent:WaitForChild("RaceGame")),
-}
-local currentMinigameIndex = 1
+--Albert Ainsten do nosso jogo
+function GameManager:StartMinigame(minigameName)
+	print("Iniciando minigame:", minigameName)
 
-local function startMinigame(minigame)
-	local minigameName = minigame.Name
-	local minigameDescription = minigame.Description
+	--abre o módulo do jogo sorteado
+	currentMinigame = require(game.ServerScriptService.Games[minigameName])
 
-	print("Iniciando minigame: " .. minigameName)
-
-	--anuncia o minigame com nome e descrição
-	if announceEvent then
-		announceEvent:FireAllClients(minigameName, minigameDescription)
+	--verifica se já está em uma conexão com o evento OnEnd para cancelar
+	if onEndConnection then
+		onEndConnection:Disconnect()
 	end
+	
+	--Vulnerabilidade: O evento OnEnd determina o numero de minigames que acontecerá depois do minigame acabar
+	--se o OnEnd for chamado 2 vezes por exemplo, vai ter 2 minigames ao mesmo tempo.
 
-	--conectar o evento OnEnd para iniciar o próximo minigame
-	minigame.OnEnd.Event:Connect(function()
+	--conectar
+	onEndConnection = currentMinigame.OnEnd.Event:Connect(function()
 		print("Evento OnEnd disparado para " .. minigameName)
-		wait(2)
 
-		--atualizar o índice e iniciar o próximo minigame
-		currentMinigameIndex = (currentMinigameIndex % #minigames) + 1
-		startMinigame(minigames[currentMinigameIndex])
+		--tempo de espera para um próximo minigame
+		GameMaster:CenterMessageLabelDisplay("Próximo Minigame em 10 segundos")
+		wait(10)
+
+		--inicia o próximo minigame após o término
+		self:StartNextMinigame()
 	end)
-
-	--inicia o minigame
-	minigame.Start()
+	
+	--inicializar o minigame
+	currentMinigame.Initialize()
+	
+	GameMaster:InfoGameDisplay(currentMinigame.Name, currentMinigame.Description) --display do nome e da descrição do jogo
+	GameMaster:CenterMessageLabelDisplay("começando "..currentMinigame.Name.." em 4s...")
+	
+	wait(4)--tempo de espera para começar o jogo
+	
+	local selectedMap = currentMinigame.Maps[math.random(1, #currentMinigame.Maps)]
+	print("Carregando o mapa:", selectedMap)
+	
+	--iniciar o minigame
+	currentMinigame.Start()
 end
 
---iniciar o primeiro minigame
-startMinigame(minigames[currentMinigameIndex])
+--próximo minigame
+function GameManager:StartNextMinigame()
+	--minigame aleatório
+	local randomMinigame = availableMinigames[math.random(1, #availableMinigames)]
+
+	self:StartMinigame(randomMinigame)
+end
+
+function GameManager:MonitorMinigames()
+	--...
+	self:StartNextMinigame()
+end
+
+--Inicia o ainsten do jogo caso o servidor seja um recém aberto, essa é a entrada de tudo
+GameManager:MonitorMinigames()
+
+return GameManager
